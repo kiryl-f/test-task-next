18c9e685b2a2c940fc83f8250178a2286bc5c5b9
Kiryl Filipovich <fkiryl@gmail.com>
2024-12-27 14:50:55 +0300

    mobile adjustments

---
+++ src/app/components/HeaderLogo.tsx
@@ -0,0 +1,17 @@
+
+import styles from './HeaderLogo.module.scss';
+import { HouseIcon } from './HouseIcon';
+
+const HeaderLogo = () => {
+  return (
+    <div className={styles.headerLogo}>
+      <HouseIcon />
+      <div className={styles.textWrapper}>
+        <p className={styles.textPrimary}>Строительные решения</p>
+        <p className={styles.textSecondary}>Строительная компания</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderLogo;
---
+++ src/app/components/HouseIcon.tsx
@@ -0,0 +1,12 @@
+export const HouseIcon = () => {
+    return (
+        <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3.5 38.5H38.5" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M5.1626 38.5002L5.2501 17.4477C5.2501 16.3802 5.7576 15.3653 6.5976 14.7003L18.8477 5.16273C20.1077 4.18273 21.875 4.18273 23.1525 5.16273L35.4027 14.6827C36.2602 15.3477 36.7502 16.3627 36.7502 17.4477V38.5002" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round" />
+            <path d="M22.75 29.75H19.25C17.7975 29.75 16.625 30.9225 16.625 32.375V38.5H25.375V32.375C25.375 30.9225 24.2025 29.75 22.75 29.75Z" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round" />
+            <path d="M16.625 24.0625H13.125C12.1625 24.0625 11.375 23.275 11.375 22.3125V19.6875C11.375 18.725 12.1625 17.9375 13.125 17.9375H16.625C17.5875 17.9375 18.375 18.725 18.375 19.6875V22.3125C18.375 23.275 17.5875 24.0625 16.625 24.0625Z" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round" />
+            <path d="M28.875 24.0625H25.375C24.4125 24.0625 23.625 23.275 23.625 22.3125V19.6875C23.625 18.725 24.4125 17.9375 25.375 17.9375H28.875C29.8375 17.9375 30.625 18.725 30.625 19.6875V22.3125C30.625 23.275 29.8375 24.0625 28.875 24.0625Z" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linejoin="round" />
+            <path d="M33.2501 12.25L33.1976 7H25.4976" stroke="#FBFBFB" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    );
+};
